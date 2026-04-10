e0aa8dadd9b2f397288755b44b2831fe70f3bf6e
DanNguyen <bidat2007@gmail.com>
2026-04-10 01:35:08 -0700

    fixed how sd card logs

--- main/main.c
+++ main/main.c
@@ -14,6 +14,7 @@
 i2c_master_bus_handle_t i2c_bus_handle;
 i2c_master_dev_handle_t sps30_dev_handle;
 i2c_master_dev_handle_t ms5611_dev_handle;
+static QueueHandle_t i2c_queue;
 
 
 /* --- SPI bus variables --- */
@@ -32,37 +33,38 @@ static Telemetry telemetry = {0};
 // Prototype for the task function
 static void i2c_task(void *arg);
 static void ze27o3_task(void *arg);
+static void telemetry_task(void *arg);
 
 void app_main(void)
 {
-    // // Initialize I2C bus
-    // i2c_master_init(&i2c_bus_handle);
-    // printf("I2C bus initialized\n");
+    // Initialize I2C bus
+    i2c_master_init(&i2c_bus_handle);
+    printf("I2C bus initialized\n");
 
     // // Add SPS30 device to the bus
     // i2c_add_device(SPS30_I2C_ADDR, &i2c_bus_handle, &sps30_dev_handle);
     // printf("SPS30 device added to I2C bus\n");
 
-    // // Add MS5611 device to the bus
-    // i2c_add_device(MS5611_I2C_ADDR, &i2c_bus_handle, &ms5611_dev_handle);
-    // printf("MS5611 device added to I2C bus\n");
+    // Add MS5611 device to the bus
+    i2c_add_device(MS5611_I2C_ADDR, &i2c_bus_handle, &ms5611_dev_handle);
+    printf("MS5611 device added to I2C bus\n");
 
     // // Start SPS30 measurement
     // ESP_ERROR_CHECK(sps30_start(sps30_dev_handle));
     // printf("SPS30 measurement started\n");
     // vTaskDelay(pdMS_TO_TICKS(50)); // Wait for measurement to start
 
-    // //Reset ms5611 sensor
-    // ms5611_reset(ms5611_dev_handle);
-    // printf("MS5611 sensor reset complete.\n");
-    // vTaskDelay(pdMS_TO_TICKS(10));           // Wait for reset to complete
-    // ms5611_read_prom(ms5611_dev_handle);     //Read device Prom values
+    //Reset ms5611 sensor
+    ms5611_reset(ms5611_dev_handle);
+    printf("MS5611 sensor reset complete.\n");
+    vTaskDelay(pdMS_TO_TICKS(10));           // Wait for reset to complete
+    ms5611_read_prom(ms5611_dev_handle);     //Read device Prom values
 
     // // Initialize ZE27O3 UART
     // ESP_ERROR_CHECK(ze27_uart_init());
 
-    // // Create a task to read I2C data periodically
-    // xTaskCreate(i2c_task, "i2c_task", 2048, &telemetry, 5, NULL);
+    i2c_queue = xQueueCreate(10, sizeof(Telemetry));
+
 
     // // Create a task to read ZE27O3 data periodically
     // xTaskCreate(ze27o3_task, "ze27o3_task", 2048, &telemetry, 5, NULL);
@@ -89,43 +91,49 @@ void app_main(void)
         printf("Failed to configure MAX31856\n");
     }
 
-    while(1){
-        if (sd_write() == ESP_OK){
-            printf("Data written to SD card successfully\n");
-        } else{
-            printf("Failed to write data to SD card\n");
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Write to SD card every 5 seconds
+    // while(1){
+    //     if (sd_write() == ESP_OK){
+    //         printf("Data written to SD card successfully\n");
+    //     } else{
+    //         printf("Failed to write data to SD card\n");
+    //     }
+    //     vTaskDelay(pdMS_TO_TICKS(1000)); // Write to SD card every 5 seconds
 
-        float thermocouple_temp = max31856_read_thermocouple_temp(&max31856_spi_handle);
-        printf("MAX31856 Thermocouple Temperature: %.2f °C\n", thermocouple_temp);
+    //     float thermocouple_temp = max31856_read_thermocouple_temp(&max31856_spi_handle);
+    //     printf("MAX31856 Thermocouple Temperature: %.2f °C\n", thermocouple_temp);
 
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Read MAX31856 every 1 second
-    }
+    //     vTaskDelay(pdMS_TO_TICKS(1000)); // Read MAX31856 every 1 second
+    // }
+
+    // Create a task to read I2C data periodically
+    xTaskCreate(i2c_task, "i2c_task", 2048, &telemetry, 5, NULL);
+
+    xTaskCreate(telemetry_task, "telemetry_task", 4096, NULL, 10, NULL);
+
 }
 
 static void i2c_task(void *arg){
     Telemetry *telemetry = (Telemetry *)arg;
     while(1){
-        // ---- SPS30 ----
-        bool SPS30_readyFlag = false;
-        esp_err_t err = sps30_ready(sps30_dev_handle, &SPS30_readyFlag);
-        if (err != ESP_OK) {
-            printf("Failed to check SPS30 readiness: %s\n", esp_err_to_name(err));
-        }
+        // // ---- SPS30 ----
+        // bool SPS30_readyFlag = false;
+        // esp_err_t err = sps30_ready(sps30_dev_handle, &SPS30_readyFlag);
+        // if (err != ESP_OK) {
+        //     printf("Failed to check SPS30 readiness: %s\n", esp_err_to_name(err));
+        // }
 
-        if (SPS30_readyFlag) {
-            err = sps30_read_pm25(sps30_dev_handle, &telemetry->SPS30_pm25);
-            if (err != ESP_OK) {
-                printf("Failed to read SPS30 PM2.5: %s\n", esp_err_to_name(err));
-            } else {
-                printf("SPS30 PM2.5: %d\n", telemetry->SPS30_pm25);
-            }
-        }else {
-            printf("SPS30 not ready yet\n");
-        }
+        // if (SPS30_readyFlag) {
+        //     err = sps30_read_pm25(sps30_dev_handle, &telemetry->SPS30_pm25);
+        //     if (err != ESP_OK) {
+        //         printf("Failed to read SPS30 PM2.5: %s\n", esp_err_to_name(err));
+        //     } else {
+        //         printf("SPS30 PM2.5: %d\n", telemetry->SPS30_pm25);
+        //     }
+        // }else {
+        //     printf("SPS30 not ready yet\n");
+        // }
 
-        vTaskDelay(pdMS_TO_TICKS(1000)); // Read sensors every 1 second
+        // vTaskDelay(pdMS_TO_TICKS(1000)); // Read sensors every 1 second
 
         // ---- MS5611 ----
         //Read D1 conversion result
@@ -136,6 +144,14 @@ static void i2c_task(void *arg){
         telemetry->MS5611_temperature = ms5611_calculateTemperature();
         telemetry->MS5611_pressure = ms5611_calculatePressure();
         printf("MS5611 Temperature: %.2f, Pressure: %.2f\n", telemetry->MS5611_temperature/100.0, telemetry->MS5611_pressure/100.0);
+        Telemetry telemetry_copy = {
+            .SPS30_pm25 = telemetry->SPS30_pm25,
+            .MS5611_temperature = telemetry->MS5611_temperature,
+            .MS5611_pressure = telemetry->MS5611_pressure,
+            .ZE27O3_o3_ppb = telemetry->ZE27O3_o3_ppb
+        };
+
+        xQueueSend(i2c_queue, &telemetry_copy, portMAX_DELAY);
 
         vTaskDelay(pdMS_TO_TICKS(1000)); // Read sensors every 1 second
     }
@@ -147,4 +163,33 @@ static void ze27o3_task(void *arg){
         ESP_ERROR_CHECK(ze27o3_readActiveUpload(&telemetry->ZE27O3_o3_ppb));
     }
 
+}
+
+static void telemetry_task(void *arg){
+    Telemetry telemetry;
+
+    while(1){
+        if(xQueueReceive(i2c_queue, &telemetry, portMAX_DELAY) == pdPASS){
+            // printf("Telemetry received from queue: PM2.5=%d, Temp=%.2f, Pressure=%.2f, O3=%d\n",
+            //        telemetry.SPS30_pm25,
+            //        telemetry.MS5611_temperature / 100.0,
+            //        telemetry.MS5611_pressure / 100.0,
+            //        telemetry.ZE27O3_o3_ppb);
+
+            // Write telemetry to SD card
+            char telemetry_data[256];
+            snprintf(telemetry_data, sizeof(telemetry_data), "PM2.5=%d, Temp=%.2f, Pressure=%.2f, O3=%d\n",
+                     telemetry.SPS30_pm25,
+                     telemetry.MS5611_temperature / 100.0,
+                     telemetry.MS5611_pressure / 100.0,
+                     telemetry.ZE27O3_o3_ppb);
+            esp_err_t err = sd_write(telemetry_data);
+            if (err != ESP_OK) {
+                printf("Failed to write telemetry to SD card: %s\n", esp_err_to_name(err));
+            } else {
+                printf("Telemetry written to SD card\n");
+            }
+            vTaskDelay(pdMS_TO_TICKS(500)); // Write every 0.5 seconds
+        }
+    }
 }
